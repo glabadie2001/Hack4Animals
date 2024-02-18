aafba73d0053d9a1fa3372b483a2416b4869e9c8
dsuszko <93233397+dsuszko@users.noreply.github.com>
2024-02-18 04:14:04 -0800

    added title screen

---
+++ Assets/StartButton.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(() => { SceneManager.LoadScene("Sandbox", LoadSceneMode.Single); });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
