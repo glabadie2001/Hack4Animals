3d91d783497a119a36450cc514abff60f3cb1fcc
dsuszko <93233397+dsuszko@users.noreply.github.com>
2024-02-17 21:17:21 -0800

    added inventory system

---
+++ Assets/Scripts/Inventory/InventoryScipt.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class InventoryScript : MonoBehaviour
+{
+    public List<InventoryItem> inventoryItems = new();
+
+    // Use this for initialization
+    void Start()
+    {
+        UIManager.inst.DrawInventory(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public bool AddItem(InventoryItem item)
+    {
+        inventoryItems.Add(item);
+        UIManager.inst.DrawInventory(this);
+        return true;
+    }
+
+    public bool RemoveItem(InventoryItem item)
+    {
+        inventoryItems.Remove(item);
+        UIManager.inst.DrawInventory(this);
+        return true;
+    }
+}
