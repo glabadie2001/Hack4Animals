89466792601a5eb52c9413342c5a182dccaad87b
JSOrozco <jsorozco252@gmail.com>
2024-02-17 23:01:53 -0800

    NPC Dialogue Interaction Implementation and basic template

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -38,7 +38,8 @@ public class GameManager : MonoBehaviour
         Physics2D.Raycast(ray.origin, ray.direction, new ContactFilter2D(), hit);
 
         if (hit[0].transform == null) return null;
+        if (hit[0].transform.parent == null) return hit[0].transform.GetComponent<Interactable>();
 
-        return hit[0].transform.GetComponent<Interactable>();
+        return hit[0].transform.GetComponentInParent<Interactable>();
     }
 }
